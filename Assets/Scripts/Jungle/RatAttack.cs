f5b27d439e1b6703c8053af0253a897f781f1880
nguyenlamhai <hainlde180364@fpt.edu.vn>
2025-10-03 02:54:26 +0700

    Enemy(Hải)

---
+++ Assets/Scripts/Jungle/RatAttack.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RatAttack : MonoBehaviour
+{
+    [SerializeField] private float knockbackForce = 10f;
+    [SerializeField] private LayerMask playerLayer;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & playerLayer) != 0)
+        {
+            Rigidbody2D playerRb = collision.GetComponent<Rigidbody2D>();
+            if (playerRb != null)
+            {
+                Vector2 direction = (collision.transform.position - transform.position).normalized;
+                playerRb.AddForce(direction * knockbackForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
